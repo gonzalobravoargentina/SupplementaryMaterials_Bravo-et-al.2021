1d45ec159618c5f49821992261c181d768879ca7
diodon <ekleins@gmail.com>
2021-07-08 18:54:19 +1000

    add FG visual robot plot fig3B

--- Code/makeFig2.R
+++ Code/makeFig2.R
@@ -202,7 +202,7 @@ pp %>% ggexport(filename = "./Figures/Fig_2B_new.png", width = 3000, height = 16
 ###############
 
 ## HUMAN ROBOT
-## go diretly to the boxplots
+## go directly to the boxplots
 FG_HR <- read_csv("./DataClean/DF_FG_HumanRobot.csv", col_types = cols())
 FG_HR$strata <- factor(FG_HR$strata, levels = c("LT", "MT", "HT"))
 FG_HR$source <- factor(FG_HR$source, levels = c("human", "visual", "robot"))
@@ -220,9 +220,28 @@ pp <- pp + geom_boxplot(position=position_dodge2(preserve = "single"), aes(fill=
 
 pp
 
-pp %>% ggexport(filename = "./Figures/Fig_2A_new.png", width = 3000, height = 1615, res = 300, pointsize = 12)
+pp %>% ggexport(filename = "./Figures/Fig_3A_new.png", width = 3000, height = 1615, res = 300, pointsize = 12)
 
 
 ## VISUAL ROBOT
+## go directly to the boxplots
+FG_VR <- read_csv("./DataClean/DF_FG_VisualRobot.csv", col_types = cols())
+FG_VR$strata <- factor(FG_VR$strata, levels = c("LT", "MT", "HT"))
+FG_VR$source <- factor(FG_VR$source, levels = c("human", "visual", "robot"))
+FG_VR$country <- factor(FG_VR$country, levels = c("AR", "CO", "EC", "US"), 
+                        labels = c("ARGENTINA", "COLOMBIA", "ECUADOR", "USA"))
+
+pp <- ggplot(FG_VR %>% filter(country != "USA"), aes(strata, Cover, fill=source))
+pp <- pp + geom_boxplot(position=position_dodge2(preserve = "single"), aes(fill=source), outlier.size = 0.3, notch = FALSE) +
+  labs(x="Stratum", y="Cover %") + 
+  scale_y_log10() + 
+  scale_x_discrete(breaks=c("HT", "MT", "LT"), labels=c("H", "M", "L")) + 
+  scale_colour_manual("Source", values = sourceColor, aesthetics = 'fill') + 
+  facet_grid(country~Label) + 
+  theme_pubclean(base_size=14)
+
+pp
+
+pp %>% ggexport(filename = "./Figures/Fig_3B_new.png", width = 3000, height = 1615, res = 300, pointsize = 12)
 
 
